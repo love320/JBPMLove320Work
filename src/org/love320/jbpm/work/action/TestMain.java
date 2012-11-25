814687e99f51fe8aa56aa210e66f174e7f36f661
ThinkPad <admin@love320.com>
2012-11-25 22:59:06 +0800

    准备导入，没有调试
    Signed-off-by: ThinkPad <admin@love320.com>

---
+++ src/org/love320/jbpm/work/action/TestMain.java
@@ -0,0 +1,12 @@
+package org.love320.jbpm.work.action;
+
+import junit.framework.TestCase;
+
+public class TestMain extends TestCase{
+
+
+	public void testAction(){
+		System.out.println(">>.");
+	}
+	
+}
